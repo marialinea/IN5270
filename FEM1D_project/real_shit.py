91f3e1526f7f2ae859c527c493dd34064027254d
Maria Linea Horgen <marialinea1@hotmail.com>
2020-10-22 19:45:03 +0200

    psi basisen er up and running

--- FEM1D_project/real_shit.py
+++ FEM1D_project/real_shit.py
@@ -19,10 +19,10 @@ class P2_FEM():
         self.element_matrix = np.zeros([2*self.Ne, 2*self.Ne])
         self.element_vector = np.array(2*Ne)
 
-        self.psi_sym = sym.zeros(self.Nn,1)  # holds symbolic basis functions
-        self.psi_num = sym.zeros(self.Nn,1)  # holds numeric basis functions
-        self.d_psi_sym = sym.zeros(self.Nn,1)  # holds symbolic derivatives of basis functions
-        self.d_psi_num = sym.zeros(self.Nn,1)  # holds numeric derivatives of basis functions
+        self.psi_sym = sym.zeros(3*self.Ne,1)  # holds symbolic basis functions
+        self.psi_num = sym.zeros(3*self.Ne,1)  # holds numeric basis functions
+        self.d_psi_sym = sym.zeros(3*self.Ne,1)  # holds symbolic derivatives of basis functions
+        self.d_psi_num = sym.zeros(3*self.Ne,1)  # holds numeric derivatives of basis functions
 
         self.x = sym.Symbol("x")
         self.h = sym.Symbol("h")
@@ -32,24 +32,27 @@ class P2_FEM():
 
 
     def BasisFunctions(self):
+        temp = -1
+        for i in range(1, 2*self.Ne-2,2):
+            self.psi_sym[temp + i] = (self.x-i*self.h)*(self.x-i*self.h-self.h)/(2*self.h*self.h)
+            self.psi_sym[temp + i+1] = -(self.x-i*self.h+self.h)*(self.x-i*self.h-self.h)/(self.h*self.h)
+            self.psi_sym[temp + i+2] = (self.x-i*self.h)*(self.x-i*self.h+self.h)/(2*self.h*self.h)
 
-        for i in range(1,2*self.Ne-2, 2):
-            self.psi_sym[i-1] = (self.x-i*self.h)*(self.x-i*self.h-self.h)/(2*self.h*self.h)
-            self.psi_sym[i] = -(self.x-i*self.h+self.h)*(self.x-i*self.h-self.h)/(self.h*self.h)
-            self.psi_sym[i+1] = (self.x-i*self.h)*(self.x-i*self.h+self.h)/(2*self.h*self.h)
+            self.d_psi_sym[temp + i] = sym.diff(self.psi_sym[i-1], self.x)
+            self.d_psi_sym[temp + i+1] = sym.diff(self.psi_sym[i], self.x)
+            self.d_psi_sym[temp + i+2] = sym.diff(self.psi_sym[i+1], self.x)
 
-            self.d_psi_sym[i-1] = sym.diff(self.psi_sym[i-1], self.x)
-            self.d_psi_sym[i] = sym.diff(self.psi_sym[i], self.x)
-            self.d_psi_sym[i+1] = sym.diff(self.psi_sym[i+1], self.x)
+            temp += 1
+
+        self.psi_sym[-3] = (self.x-1+self.h)*(self.x-1)/(2*self.h*self.h)
+        self.psi_sym[-2] = -(self.x-1+2*self.h)*(self.x-1)/(self.h*self.h)
+        self.psi_sym[-1] = (self.x-1+self.h)*(self.x-1+2*self.h)/(2*self.h*self.h)
 
 
-        self.psi_sym[2*self.Ne-2] = (self.x-1+self.h)*(self.x-1)/(2*self.h*self.h)
-        self.psi_sym[2*self.Ne-1] = -(self.x-1+2*self.h)*(self.x-1)/(self.h*self.h)
-        self.psi_sym[2*self.Ne] = (self.x-1+self.h)*(self.x-1+2*self.h)/(2*self.h*self.h)
-
-        self.d_psi_sym[2*self.Ne-2] = sym.diff(self.psi_sym[2*self.Ne-2], self.x)
-        self.d_psi_sym[2*self.Ne-1] = sym.diff(self.psi_sym[2*self.Ne-1], self.x)
-        self.d_psi_sym[2*self.Ne] = sym.diff(self.psi_sym[2*self.Ne], self.x)
+        self.d_psi_sym[-3] = sym.diff(self.psi_sym[2*self.Ne-2], self.x)
+        self.d_psi_sym[-2] = sym.diff(self.psi_sym[2*self.Ne-1], self.x)
+        self.d_psi_sym[-1] = sym.diff(self.psi_sym[2*self.Ne], self.x)
+        print(self.psi_sym)
 
         if not self.symbolic:
             self.psi_num = [sym.lambdify([self.x,self.h], self.psi_sym[i], modules="numpy") for i in range(self.Nn)]
